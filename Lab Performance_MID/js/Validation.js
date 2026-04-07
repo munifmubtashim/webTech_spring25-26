2d4b4badd6a4f132c101b431e54fda4291abf440
munifmubtashim <munifmubtasim@gmail.com>
2026-04-07 19:35:12 +0600

    static data

---
+++ Lab Performance_MID/js/Validation.js
@@ -0,0 +1,6 @@
+const price = document.getElementById('uprice');
+const quantity = document.getElementById("qperday");
+const total = document.getElementById("tprice");
+const unit = 1000;
+const days = 30;
+
